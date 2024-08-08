dc37de434d55355ed8b91e8042fd82f4e6d14a07
EbrimaHydara <ehydaradev@gmail.com>
2024-08-08 11:03:26 +0900

    AutoLoc on 2024-08-08

--- AutoLoc/ai_localizers/ai_loc_print.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-api_key= "sk-proj-vUbzJ4KnwGp7AohPrBTST3BlbkFJrTTxQ6M3sk9VUaggFNJI"
-client = OpenAI(api_key=api_key)
-print(client.models.list().data)
-
-# "C:\Users\ts-ebrima.hydara\Documents\SSL Certs\combined_cert 1.pem"
-# "C:\Users\ts-ebrima.hydara\Documents\SSL Certs\combined_ssl_cert 1.pem"
